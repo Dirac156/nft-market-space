1f4362f2efd72dfcb18f6ca50e4a27843e795028
Dirac Murairi <dmurairimukongya@gmail.com>
2022-08-19 01:33:47 +0200

    error handling on ERC721Enumerable contract

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -16,28 +16,6 @@ contract ERC721Enumerable is ERC721 {
     // mapping from token ID index to index of the owner tokens list
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
-    /// @notice count NFTs tracked by thus contract
-    /// @return uint256 A count of valid NFTs tracked by this contract,
-    function totalSupply() external view returns (uint256) {
-        return _allTokens.length;
-    }
-
-    /// @notice Enumerate valid NFTs
-    /// @dev throws if `_index` >= `totalSupply()`.
-    /// @param _index A counter less than `totoalSupply`
-    /// @return uint256 The token identifier for the `_index`th NFT,
-    /// (sort order not specified)
-    function tokenByIndex(uint256 _index) external view returns(uint256) {}
-
-    function tokenOfOwnerByIndex(uint256 _index) external view returns(uint256){}
-
-    // overide _mint from ERC721
-    function _mint(address to, uint256 tokenId) internal override(ERC721) {
-        super._mint(to, tokenId);
-        _addTokenToAllTokensToEnumerable(tokenId);
-        _addTokenToOwnerEnumeration(to, tokenId);
-    }
-
     /// @notice add tokens to the _allTokens array 
     /// and set the position of the token indexes
     function _addTokenToAllTokensToEnumerable(uint256 tokenId) private {
@@ -53,4 +31,34 @@ contract ERC721Enumerable is ERC721 {
         _ownedTokensIndex[tokenId] = _ownedTokens[owner].length;
         _ownedTokens[owner].push(tokenId);
     }
+
+    /// @notice count NFTs tracked by thus contract
+    /// @return uint256 A count of valid NFTs tracked by this contract,
+    function totalSupply() public view returns (uint256) {
+        return _allTokens.length;
+    }
+
+    /// @notice Enumerate valid NFTs
+    /// @dev throws if `index` >= `totalSupply()`.
+    /// @param index A counter less than `totoalSupply`
+    /// @return uint256 The token identifier for the `_index`th NFT,
+    /// (sort order not specified)
+    function tokenByIndex(uint256 index) external view returns(uint256) {
+        // make sure thr index is not out of range
+        require(index < totalSupply(), 'global index is out of bounds');
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns(uint256){
+        require(index < balanceOf(owner), 'owner index is out of bounds');
+        return _ownedTokens[owner][index];
+    }
+
+    // overide _mint from ERC721
+    function _mint(address to, uint256 tokenId) internal override(ERC721) {
+        super._mint(to, tokenId);
+        _addTokenToAllTokensToEnumerable(tokenId);
+        _addTokenToOwnerEnumeration(to, tokenId);
+    }
+
 }
